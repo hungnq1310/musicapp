9930474b8042ef940fee284b0c09be82a1115cf1
hungnq131002 <hungnq131002@gmail.com>
2025-01-08 15:27:54 +0700

    init default style

---
+++ src/styles/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import {colors, fontSize } from '@/constants/tokens';
+
+// default themes
+const defaultStyles = StyleSheet.create({
+    container: {
+        flex:1, backgroundColor: colors.background,
+    },
+
+    text: {
+        fontSize: fontSize.base,
+        color: colors.text,
+    }
+
+});
+
+// use later
+const utilsStyles = StyleSheet.create({
+    // ...
+});
