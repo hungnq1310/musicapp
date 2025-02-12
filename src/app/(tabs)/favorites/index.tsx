0f5ee6838f50afc7fea3c7e0526fa758c0346cfb
hungnq131002 <hungnq131002@gmail.com>
2025-02-12 14:26:53 +0700

    feat: add other tabs

---
+++ src/app/(tabs)/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { defaultStyles } from '@/styles'
+import { View, Text } from 'react-native'
+
+const FavoriteScreens = () => {
+    return (
+        <View style={defaultStyles.container}>
+            <Text>Favorites Screens</Text>
+        </View>
+    )
+}
+
+export default FavoriteScreens;
