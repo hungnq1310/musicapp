7a18d358fe9e0f1e4a4496f8b586cc6da3074647
hungnq131002 <hungnq131002@gmail.com>
2025-02-12 14:43:05 +0700

    design background view

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,8 +1,38 @@
-import { Tabs } from 'expo-router'
+import { colors, fontSize } from '@/constants/tokens';
+import { BlurView } from 'expo-blur';
+import { Tabs } from 'expo-router';
+import { StyleSheet } from 'react-native';
 
 const TabsNavigation = () => {
 	return (
-		<Tabs>
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: colors.primary,
+				tabBarLabelStyle: { 
+					fontSize: fontSize.xs,
+					fontWeight: 500, 
+				},
+				tabBarStyle: {
+					position: 'absolute',
+					borderTopLeftRadius: 20,
+					borderTopRightRadius: 20,
+					borderTopWidth: 0,
+					paddingTop: 8,
+				},
+				headerShown: false,
+				tabBarBackground: () => <BlurView 
+					intensity={95}
+					style={{
+						...StyleSheet.absoluteFillObject,
+						overflow: 'hidden',
+						borderTopLeftRadius: 20,
+						borderTopRightRadius: 20,
+
+					}}
+				/>	
+				
+			}}
+		>
 			<Tabs.Screen name="favorites" />
 			<Tabs.Screen name="playlists" />
 			<Tabs.Screen name="(songs)" />
