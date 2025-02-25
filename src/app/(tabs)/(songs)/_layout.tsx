8d44b89fce771498e8350317e1b64115e45f0e0e
hungnq131002 <hungnq131002@gmail.com>
2025-02-25 09:43:44 +0700

    chores: update layout

--- src/app/(tabs)/(songs)/_layout.tsx
+++ src/app/(tabs)/(songs)/_layout.tsx
@@ -1,12 +1,17 @@
 import { View } from 'react-native'
 import { defaultStyles } from '@/styles'
 import { Stack } from 'expo-router'
+import { StackScreenWithSearchBar } from '@/constants/layout'
 
 const SongsScreenLayout = () => {
 	return (
 		<View style={defaultStyles.container}>
 			<Stack>
-				<Stack.Screen name="index" options={{ headerTitle: 'Songs' }} />
+				<Stack.Screen name="index" options={{ 
+					headerTitle: 'Songs',
+					...StackScreenWithSearchBar,	
+
+				}} />
 			</Stack>
 		</View>
 	)
--- src/app/(tabs)/playlists/_layout.tsx
+++ src/app/(tabs)/playlists/_layout.tsx
@@ -1,12 +1,16 @@
 import { View } from 'react-native'
 import { defaultStyles } from '@/styles'
 import { Stack } from 'expo-router'
+import { StackScreenWithSearchBar } from '@/constants/layout'
 
 const PlaylistsScreenLayout = () => {
     return (
         <View style={defaultStyles.container}>
             <Stack>
-                <Stack.Screen name="index" options={{ headerTitle: 'Playlists' }} />
+                <Stack.Screen name="index" options={{ 
+                    headerTitle: 'Playlists', 
+                    ...StackScreenWithSearchBar,
+                }} />
             </Stack>
         </View>
     )
